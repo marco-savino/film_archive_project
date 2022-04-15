277e13bc782fec72a8b51edca9f6b2edd38d199c
mark-sawine <marcosavino@tutanota.com>
2022-04-15 12:50:55 +0200

    FilmController.Get is able to connenct to MySql DB, to read all the records and to return them in a rudimental string format

--- backend/Controllers/FilmController.cs
+++ backend/Controllers/FilmController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using backend.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,10 +10,42 @@ namespace backend.Controllers
     [ApiController]
     public class FilmController : ControllerBase
     {
+        private archivio_filmContext ctx = new archivio_filmContext();
         // GET: api/<FilmController>
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            List<string> fieldList = new List<string>();
+            MySqlConnection connection = new MySqlConnection();
+            connection.ConnectionString = "server=localhost;database=archivio_film;uid=localUser;pwd=localUser";
+            try{
+                connection.Open();
+                Console.WriteLine("Connessione eseguita");
+
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT * FROM film";
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                { 
+                    fieldList.Add(reader["id"].ToString());
+                    fieldList.Add(reader["titolo"].ToString());
+                    fieldList.Add(reader["genere"].ToString());
+                    fieldList.Add(reader["durata"].ToString());
+                }
+
+                return fieldList.ToArray();
+
+            }catch(Exception e){
+                Console.WriteLine("Eccezione!\n "+ e.Message);
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                    connection.Close();
+            }
+
+
+
             return new string[] { "value1", "value2" };
         }
 
